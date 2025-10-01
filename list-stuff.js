fb0c79da8e601ad416c61574eb0f3c30cac14a51
Chris B. Behrens <cbehrens@gmail.com>
2025-10-01 13:44:57 -0500

    m1-c6 complete

--- list-stuff.js
+++ list-stuff.js
@@ -49,3 +49,38 @@ var dateList = new DateList();
 dateList.add(new Date(2025, 0, 1)); // January 1, 2025
 dateList.add(new Date(2025, 11, 31)); // December 31, 2025
 console.log("Date List:", dateList.get(0));
+var ListImpl = /** @class */ (function () {
+    function ListImpl() {
+        this.items = [];
+    }
+    ListImpl.prototype.add = function (item) {
+        this.items.push(item);
+    };
+    ListImpl.prototype.get = function (index) {
+        return this.items[index];
+    };
+    return ListImpl;
+}());
+var stringList2 = new ListImpl();
+stringList2.add("Hello");
+stringList2.add("World");
+console.log("String List:");
+console.log("Index 0:", stringList2.get(0)); // "Hello"
+console.log("Index 1:", stringList2.get(1)); // "World"
+console.log("Index 2:", stringList2.get(2112)); // undefined (implicit)
+// Number List
+var numberList2 = new ListImpl();
+numberList2.add(2112);
+numberList2.add(3.14);
+console.log("Number List:");
+console.log("Index 0:", numberList2.get(0)); // 2112
+console.log("Index 1:", numberList2.get(1)); // 3.14
+console.log("Index 2:", numberList2.get(2113)); // undefined (implicit)
+// Date List
+var dateList2 = new ListImpl();
+dateList2.add(new Date(2025, 0, 1)); // January 1, 2025
+dateList2.add(new Date(2025, 11, 31)); // December 31, 2025
+console.log("Date List:");
+console.log("Index 0:", dateList2.get(0)); // 2025-01-01T00:00:00.000Z
+console.log("Index 1:", dateList2.get(1)); // 2025-12-31T00:00:00.000Z
+console.log("Index 2:", dateList2.get(2)); // undefined (implicit)
--- list-stuff.ts
+++ list-stuff.ts
@@ -52,3 +52,39 @@ const dateList: List<Date> = new DateList();
 dateList.add(new Date(2025, 0, 1)); // January 1, 2025
 dateList.add(new Date(2025, 11, 31)); // December 31, 2025
 console.log("Date List:", dateList.get(0));
+
+class ListImpl<T> {
+    private items: T[] = [];
+    add(item: T): void {
+        this.items.push(item);
+    }
+    get(index: number): T | undefined {
+        return this.items[index];
+    }
+}
+
+const stringList2 = new ListImpl<string>();
+stringList2.add("Hello");
+stringList2.add("World");
+console.log("String List:");
+console.log("Index 0:", stringList2.get(0)); // "Hello"
+console.log("Index 1:", stringList2.get(1)); // "World"
+console.log("Index 2:", stringList2.get(2112)); // undefined (implicit)
+
+// Number List
+const numberList2 = new ListImpl<number>();
+numberList2.add(2112);
+numberList2.add(3.14);
+console.log("Number List:");
+console.log("Index 0:", numberList2.get(0)); // 2112
+console.log("Index 1:", numberList2.get(1)); // 3.14
+console.log("Index 2:", numberList2.get(2113)); // undefined (implicit)
+
+// Date List
+const dateList2 = new ListImpl<Date>();
+dateList2.add(new Date(2025, 0, 1)); // January 1, 2025
+dateList2.add(new Date(2025, 11, 31)); // December 31, 2025
+console.log("Date List:");
+console.log("Index 0:", dateList2.get(0)); // 2025-01-01T00:00:00.000Z
+console.log("Index 1:", dateList2.get(1)); // 2025-12-31T00:00:00.000Z
+console.log("Index 2:", dateList2.get(2)); // undefined (implicit)
